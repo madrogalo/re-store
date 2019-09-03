8775cc28c471d0a42d652a1ee64f7589e01cdb2d
ievhenii <youn4ik@gmail.com>
2019-09-03 23:19:05 +0300

    fix bugs

--- src/store.js
+++ src/store.js
@@ -2,6 +2,26 @@ import { createStore } from 'redux';
 
 import reducer from './reducers';
 
-const store = createStore(reducer);
+const logMiddleware = (store) => (dispatch) => (action) => {
+  console.log(action.type);
+  return dispatch(action)
+};
+
+const enhancer = (createStore) => (...args) => {
+  const store = createStore(...args)
+  const originalDispatch = store.dispatch;
+  store.dispatch = ( action ) => {
+    if (typeof action === 'string') {
+     return originalDispatch({
+        type: action
+      });
+    }
+  
+    return originalDispatch(action);
+  };
+  return store;
+};
+
+const store = createStore(reducer, enhancer);
 
 export default store;
